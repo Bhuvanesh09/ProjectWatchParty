14dbdb8ab5a20fe8c688a881b2115753b01e66e9
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2021-04-08 10:47:57 +0530

    Added firebase 8.3.2 static files directly into service worker Now need to send messages from popup js into service worker to handle connections

--- background.js
+++ background.js
@@ -4,3 +4,8 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+(function() {
+    self.importScripts("firebase/firebase-app.js", "firebase/firebase-analytics.js");
+    console.log(firebase); // should be truthy
+})();
