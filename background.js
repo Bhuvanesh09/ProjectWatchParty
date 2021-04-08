0405ce391ff9528bb8e05cccd130feb52e6a6521
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2021-04-08 11:04:20 +0530

    Thou must have ESLint in a JS project

--- background.js
+++ background.js
@@ -1,11 +1,21 @@
-let color = '#3aa757';
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+        apiKey: "AIzaSyAymlhRxTn9Vpc5BYC7xrT7iOJ-wtbX5ec",
+        authDomain: "simulated-hangouts.firebaseapp.com",
+        projectId: "simulated-hangouts",
+        storageBucket: "simulated-hangouts.appspot.com",
+        messagingSenderId: "17379851992",
+        appId: "1:17379851992:web:a4f7e957c910926c58a1ad",
+        measurementId: "G-RNGS5Q6MQ3",
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
 });
 
-(function() {
+(function () {
     self.importScripts("firebase/firebase-app.js", "firebase/firebase-analytics.js");
     console.log(firebase); // should be truthy
-})();
+}());
